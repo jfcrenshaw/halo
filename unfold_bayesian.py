79c9ed0eefa7999764913893ed410ea1ac8919ec
John Franklin Crenshaw <jfcrenshaw@gmail.com>
2018-09-21 11:11:19 -0400

    added __init__.py in modules folder so i can run scripts on linux.

--- unfold_bayesian.py
+++ unfold_bayesian.py
@@ -5,4 +5,9 @@ from modules.truth import truth
 inputfile = "input.txt"
 truth = truth(inputfile) # dictionary of inputs
 
-print(truth)
+# Bayesian Unfolding
+for config in truth:
+	for pair in truth[config]:
+		config_num = config[4]
+		config_dist = config[6:-3]
+		print(config_num,config_dist)
